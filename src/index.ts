6211f3bb5e2c9416ef47796e5972993146fe2d10
handwoo24 <handwoo24@gmail.com>
2023-07-06 16:14:33 +0900

    :wrench: fix export import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-export { default as KakaoProvider } from './KakaoProvider'
-export {
-  default as authorizeKakaoUser,
-  getKakaoToken,
-  getKakaoUser,
-  type KakaoUser,
-  type KakaoTokenOptions,
-} from './authorize-kakao'
+export { default as KakaoScript } from './KakaoScript'
+export { default as KakaoMapScript } from './KakaoMapScript'
+export type * from './interface'
+export { default as authorizeKakaoUser, getKakaoToken, getKakaoUser } from './authorize-kakao'
