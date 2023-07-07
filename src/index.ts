107595d78149ae783b9b3ba5ccd27838e6b70b36
handwoo24 <handwoo24@gmail.com>
2023-07-07 18:08:38 +0900

    Hotfix 1.3.5

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type * from './interface'
-export { default as KakaoAuthProvider } from './KakaoAuthProvider'
-export { default as KakaoMapProvider } from './KakaoMapProvider'
+export { default as KakaoAuthProvider, useKakaoAuth } from './KakaoAuthProvider'
+export { default as KakaoMapProvider, useKakaoMap } from './KakaoMapProvider'
 export { default as KakaoMap } from './KakaoMap'
 export { default as authorizeKakaoUser, getKakaoToken, getKakaoUser } from './authorize-kakao'
