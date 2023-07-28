cdf40ba994d56fe1cb4b46a98cd41eb9fe039fe0
handwoo24 <81428531+handwoo24@users.noreply.github.com>
2023-07-28 09:33:16 +0900

    Release1.6.1 (#7)
    * :wrench: add method
    
    * :sparkles: bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export type * from './interface'
 export { default as KakaoMap } from './KakaoMap'
-export { getKakaoToken, getKakaoUser, initKakaoAuth, authorizeKakaoAuth } from './authorize'
+export {
+  getKakaoToken,
+  getKakaoUser,
+  initKakaoAuth,
+  authorizeKakaoAuth,
+  setKakaoAccessToken,
+  logoutKakao,
+} from './authorize'
 export { loadKakaoMap } from './map'
 export { default as KakaoAuthorizeButton } from './KakaoAuthroizeButton'
